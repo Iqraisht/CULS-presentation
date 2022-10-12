eec19aba1846fef2b3c68c1707e4d69fa2dc00f3
Iqra Ishtiaq <104334598+Iqraisht@users.noreply.github.com>
2022-10-12 12:54:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-var slide = 0;
+let chapter = 0;
 const buttons = document.getElementById("tools").getElementsByClassName("buttons")[0];
 const nextSlide = document.getElementById("nextSlide");
 const previousSlide = document.getElementById("previousSlide");
+const nextChapter = document.getElementById("nextChapter");
+const previousChapter = document.getElementById("previousChapter");
 const restart = document.getElementById("restart");
-const chapter = document.getElementsByClassName("chapter")[0];
-const chapterLength = parseInt(chapter.style.width.slice(0, -4));
-const title = document.querySelector("#titles > span:first-child");
+const chapters = document.getElementsByClassName("chapter");
+const chapterLengths = chapters.map((chapter) => parseInt(chapter.style.width.slice(0, -4)));
+const slides = chapters.map((chapter) => 0);
+const titles = document.querySelectorAll("#titles > span");
 
+function moveChapter(amount) {
+  chapter += amount;
+  if (chapter < 0) {
+    chapter = 0;
+  } else if (chapter == chapterLengths[chapter]) {
+    chapter = chapterLengths[chapter] - 1;
+  }
+  titles[chapter].querySelector(".position").innerHTML = slides[chapter] + 1;
+  chapter.style.translate = "-" + slides[chapter] + "00vw" + chapter + "00vh";
+}
 
 function moveSlide(amount) {
-  slide += amount;
-  if (slide < 0) {
-    slide = 0;
+  
+  slides += amount;
+  if (slides < 0) {
+    slides = 0;
   } else if (slide == chapterLength) {
-    slide = chapterLength - 1;
+    slides = chapterLength - 1;
   }
-  title.querySelector(".position").innerHTML = slide + 1;
-  chapter.style.translate = "-" + slide + "00vw 0";
+  title.querySelector(".position").innerHTML = slides + 1;
+  chapter.style.translate = "-" + slides + "00vw 0";
 }
 
 function fadeButtons() {
@@ -29,7 +43,9 @@ function unfadeButtons(e) {
 
 nextSlide.addEventListener("click", (e) => { moveSlide(1) });
 previousSlide.addEventListener("click", (e) => { moveSlide(-1) });
-restart.addEventListener("click", (e) => { moveSlide(- slide) });
+nextChapter.addEventListener("click", (e) => { moveChapter(1) });
+previousChapter.addEventListener("click", (e) => { moveChapter(-1) });
+restart.addEventListener("click", (e) => { moveSlide(- slides) });
 buttons.addEventListener("mouseenter", unfadeButtons);
 buttons.addEventListener("mouseleave", fadeButtons);
 title.style.display = "inline";
