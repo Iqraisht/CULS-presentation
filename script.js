39f5b81581fba441788ab2e24039fe4ce644bce0
Iqra Ishtiaq <104334598+Iqraisht@users.noreply.github.com>
2022-10-13 10:52:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,25 +12,45 @@ const chapterLengths = chaptersArr.map((chapter) => parseInt(chapter.style.width
 const slides = chaptersArr.map((chapter) => 0);
 const titles = document.querySelectorAll("#titles > span");
 
+function setPosition() {
+  localStorage.setItem("chapter", chapter);
+  localStorage.setItem("slide", slides[chapter]);  
+}
+
 function moveChapter(amount) {
   titles[chapter].style.display = "none";
   chapter += amount;
-  if (chapter < 0) {
+  if (chapter <= 0) {
+    previousChapter.disabled = true;
     chapter = 0;
-  } else if (chapter == chapterLengths[chapter]) {
-    chapter = chapterLengths[chapter] - 1;
+  } else if (chapter >= (chapterLengths.length - 1)) {
+    nextChapter.disabled = true;
+    chapter = chapterLengths.length - 1;
+  } else {
+    nextChapter.disabled = false;
+    previousChapter.disabled = false;    
   }
   titles[chapter].querySelector(".position").innerHTML = slides[chapter] + 1;
   titles[chapter].style.display = "inline";
   presentation.style.translate = "-" + slides[chapter] + "00vw -" + chapter + "00vh";
+  if (!(amount == 0)) setPosition();
 }
 
 function moveSlide(amount) {
   slides[chapter] += amount;
-  if (slides[chapter] < 0) {
+  if (slides[chapter] <= 0) {
+    previousSlide.disabled = true;
     slides[chapter] = 0;
-  } else if (slides[chapter] == chapterLengths[chapter]) {
+  } else if (slides[chapter] >= (chapterLengths[chapter] - 1)) {
+    nextSlide.disabled = true;
     slides[chapter] = chapterLengths[chapter] - 1;
+  } 
+  if (!(slides[chapter] == 0))  {
+    previousSlide.disabled = false;
+        
+  }
+  if (!(slides[chapter] == chapterLengths[chapter] - 1)) {
+    nextSlide.disabled = false;
   }
   moveChapter(0);
 }
@@ -40,33 +60,21 @@ function fadeButtons() {
 }
 
 function unfadeButtons(e) {
+  clearTimeout(t);
   buttons.style.opacity = 1; 
 }
 
 nextSlide.addEventListener("click", (e) => { moveSlide(1) });
 previousSlide.addEventListener("click", (e) => { moveSlide(-1) });
-nextChapter.addEventListener("click", (e) => { moveChapter(1) });
-previousChapter.addEventListener("click", (e) => { moveChapter(-1) });
+nextChapter.addEventListener("click", (e) => { moveChapter(1); moveSlide(0); });
+previousChapter.addEventListener("click", (e) => { moveChapter(-1); moveSlide(0); });
 restart.addEventListener("click", (e) => {  moveChapter(- chapter); moveSlide(- slides[chapter]); });
 buttons.addEventListener("mouseenter", unfadeButtons);
 buttons.addEventListener("mouseleave", fadeButtons);
 titles[chapter].style.display = "inline";
-setTimeout(fadeButtons, 2500);
-
-function fadeButtons() {
-  buttons.style.opacity = 0.05;
-}
-
-function unfadeButtons(e) {
-  buttons.style.opacity = 1; 
-}
-
-nextSlide.addEventListener("click", (e) => { moveSlide(1) });
-previousSlide.addEventListener("click", (e) => { moveSlide(-1) });
-nextChapter.addEventListener("click", (e) => { moveChapter(1) });
-previousChapter.addEventListener("click", (e) => { moveChapter(-1) });
-restart.addEventListener("click", (e) => { moveSlide(- slides) });
-buttons.addEventListener("mouseenter", unfadeButtons);
-buttons.addEventListener("mouseleave", fadeButtons);
-title.style.display = "inline";
-setTimeout(fadeButtons, 2500);
+const t = setTimeout(fadeButtons, 2500);
+const slide = parseInt(localStorage.getItem("slide"));
+const localChapter = parseInt(localStorage.getItem("chapter"));
+if (localChapter) { moveChapter(localChapter) };
+if (slide) { moveSlide(slide) }
+else { moveSlide(0) }
